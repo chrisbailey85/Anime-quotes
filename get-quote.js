88dc764cd630c18349fa08a624cf43b4f27e2875
Chris Bailey <chris85bailey@aol.co.uk>
2020-10-13 10:27:30 +0100

    update url const to new fetch route

--- get-quote.js
+++ get-quote.js
@@ -1,9 +1,9 @@
-const url = "https://anime-chan.herokuapp.com/api/quotes/random"
+const url = "https://animechanapi.xyz/api/quotes/random"
 const getQuote = function () {
     fetch(url)
         .then(res => res.json())
         .then(res => {
-            let quote = { ...res[0] }
+            let quote = { ...res.data[0] }
             let characters = quote.anime + quote.quote + quote.character;
             if (characters.length > 267) {
                 console.log("too long")
@@ -16,7 +16,6 @@ const getQuote = function () {
             const tweet = quote.anime + " " + '"' + quote.quote + '" ' + quote.character
             document.querySelector(".twitter-share-button").setAttribute("href", "https://twitter.com/intent/tweet?hashtags=animequotes&text=" + encodeURIComponent(tweet))
         })
-
 }
 
 
